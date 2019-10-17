159a7beb5c86b3cbc1145db9ecf9be8b78d4020d
Rafael Soares <rafaelsoares01@gmail.com>
2019-10-17 17:21:38 -0300

    Commit first application in C#

---
+++ CalculadoraPintura/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraPintura
+{
+    class Calculadora
+    {
+        private double areaParedes;
+        private double areaTeto;
+
+        public double CalcularAreaParedes(double largura, double profundidade, double altura)
+        {
+            areaParedes = 2 * (largura + profundidade) * altura;
+            return areaParedes;
+        }
+
+        public double CalcularAreaTeto(double largura, double profundidade)
+        {
+            areaTeto = largura * profundidade;
+            return areaTeto;
+        }
+
+        public double CalcularLitragem()
+        {
+            return (areaParedes + areaTeto) / 10;
+        }
+    }
+}
